36f179bd6e80b0e9ea98e35e9f78e2ee5c9cea47
Emran Hossain <emranhossain0335@gmail.com>
2024-11-26 21:16:17 +0600

    class is explained in details in python

--- objects.py
+++ objects.py
@@ -51,3 +51,73 @@ print(car3.color)
 print(car3.for_sale)
 car3.drive()
 car3.stop()
+
+
+# class variables = Shared among all instances(created objects) of a class
+#                   Defined outside the constructor(def __init__(self))
+#                   Allow you to share data among all objects created from that class
+
+# instance variables have their own version.
+# but class variables shared among all instances(objects created)
+
+class Bird:
+    wings = 2   # class variables
+
+    def __init__(self, name, isPopular):
+        self.name = name                # instance variable
+        self.isPopular = isPopular      # instance variable
+
+    def speak(selt):
+        print(f"The {selt.name} can speak")
+
+
+crow = Bird("Crow", True)
+
+print(crow.name)
+crow.speak()
+
+
+class Student:
+
+    # class variables
+    class_year = 2024
+    num_students = 0
+
+    def __init__(self, name, age):
+        # instances variables
+        self.name = name
+        self.age = age
+        # self refers to the instance or object we are currently working with
+        Student.num_students += 1
+
+
+student1 = Student("Emran", 36)
+student2 = Student("Hossain", 35)
+student3 = Student("Sadifa", 3)
+student4 = Student("Nusaiba", 3)
+
+print(student1.name)
+print(student2.name)
+print(student3.name)
+
+print(student1.age)
+print(student2.age)
+print(student3.age)
+
+# But it is not a good practice.
+print(student1.class_year)
+print(student2.class_year)
+print(student3.class_year)
+
+# The good practice is to access class variable by the name of the class
+print(Student.class_year)  # it is more explicit
+print(Student.num_students)
+# Number of students is 4 because it three instances(student-objects) are created.
+
+
+# selt = student1 or student2 or student3. So, self refers to the object(instance)
+# def __init__(self, name, age):
+#     # instances variables
+#     student1.name = name
+#     student1.age = age
+#     # self refers to the instance or object we are currently working with
